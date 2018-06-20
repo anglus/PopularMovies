bebd0365b52b176d67fafa1a43927a2e65056abc
Matthew Morris <anglus@gmail.com>
2018-06-19 23:04:19 -0500

    Create MovieAdapter to handle RecyclerView and move network calls from MainActivity to MovieAdapter.

--- app/src/main/java/net/anglus/popularmovies/activity/MainActivity.java
+++ app/src/main/java/net/anglus/popularmovies/activity/MainActivity.java
@@ -2,52 +2,31 @@ package net.anglus.popularmovies.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.TextView;
 
 import net.anglus.popularmovies.BuildConfig;
 import net.anglus.popularmovies.R;
-import net.anglus.popularmovies.model.MovieResponse;
-import net.anglus.popularmovies.rest.MovieClient;
-import net.anglus.popularmovies.rest.MovieService;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import net.anglus.popularmovies.adapter.MovieAdapter;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final String API_KEY = BuildConfig.API_KEY;
+    private static final int NUM_LIST_ITEMS = 20;
+
+    private MovieAdapter mAdapter;
+    private RecyclerView mMovieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TextView key = findViewById(R.id.api_key);
-        //key.setText(API_KEY);
-        MovieService service = MovieClient.getRetrofit().create(MovieService.class);
 
-        //Call<MovieResponse> call = service.getMovie(129, API_KEY);
-        Call<MovieResponse> call = service.getMoviesByPopularity(API_KEY);
-        call.enqueue(new Callback<MovieResponse>() {
-            @Override
-            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
-                Integer page = response.body().getPage();
-                Integer totalPages = response.body().getTotalPages();
-                Integer totalResults = response.body().getTotalResults();
-                String results = response.body().getResults();
-                String movies = "page: " + page +
-                                "\n\ntotal pages: " + totalPages +
-                                "\n\ntotal results: " + totalResults +
-                                "\n\nresults: \n" + results;
-                TextView mResults = findViewById(R.id.hello);
-                mResults.setText(movies);
-            }
-
-            @Override
-            public void onFailure(Call<MovieResponse> call, Throwable t) {
-                Log.e(TAG, t.toString());
-            }
-        });
+        mMovieList = (RecyclerView) findViewById(R.id.rv_main);
+        mMovieList.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+        mMovieList.setHasFixedSize(true);
+        mAdapter = new MovieAdapter(NUM_LIST_ITEMS);
+        mMovieList.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/net/anglus/popularmovies/adapter/MovieAdapter.java
+++ app/src/main/java/net/anglus/popularmovies/adapter/MovieAdapter.java
@@ -1,4 +1,87 @@
 package net.anglus.popularmovies.adapter;
 
-public class MovieAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.util.Log;
+
+import net.anglus.popularmovies.BuildConfig;
+import net.anglus.popularmovies.R;
+import net.anglus.popularmovies.rest.MovieClient;
+import net.anglus.popularmovies.rest.MovieService;
+import net.anglus.popularmovies.model.MovieResponse;
+import net.anglus.popularmovies.model.Movie;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private static final String TAG = MovieAdapter.class.getSimpleName();
+    private static final String API_KEY = BuildConfig.API_KEY;
+
+    private int mNumberItems;
+
+    public MovieAdapter(int numberOfItems) {
+        mNumberItems = numberOfItems;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int listItemId = R.layout.movie_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(listItemId, viewGroup, shouldAttachToParentImmediately);
+        MovieViewHolder viewHolder = new MovieViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder viewHolder, int position) {
+        viewHolder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+        TextView movieItemView;
+        List<Movie> movieList;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+
+            movieItemView = (TextView) itemView.findViewById(R.id.tv_movie);
+        }
+
+        void bind (final int movieIndex) {
+            MovieService service = MovieClient.getRetrofit().create(MovieService.class);
+
+            Call<MovieResponse> call = service.getMoviesByPopularity(API_KEY);
+            call.enqueue(new Callback<MovieResponse>() {
+                @Override
+                public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                    movieList = response.body().getResults();
+                    String movieItem = movieList.get(movieIndex).toString();
+                    movieItemView.setText(movieItem);
+                }
+
+                @Override
+                public void onFailure(Call<MovieResponse> call, Throwable t) {
+                    Log.e(TAG, t.toString());
+                }
+            });
+        }
+    }
 }
